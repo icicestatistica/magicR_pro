49572edd8b57e9da1eece39d5297d0d7d6c5ff84
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-22 09:49:54 -0300

    Update cat_same_levels.R

--- R/cat_same_levels.R
+++ R/cat_same_levels.R
@@ -6,7 +6,7 @@ if(nas==T) levels=c(levels,"N/A")
 names(df)=c("Variável",levels)
 return(df)}
 
-cat_same_levels_2 <- function(x,nomes,levels,nas,dig){
+cat_same_levels_2 <- function(x,nomes,levels,dig){
 prop=prop.test(table(factor(x[,1],levels=levels)))
 IC=paste0("(",round(100*prop$conf.int[1],dig),"%, ",round(100*prop$conf.int[2],dig),"%)")
 df = data.frame("Variável"=nomes[1],desc_uni_categorica(x[,1],levels,F,F,F,F,dig)[1,c(2,3)],IC)
