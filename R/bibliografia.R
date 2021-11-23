1830bff16c6f9a2ee85b1db61270ffc828b2e265
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-23 02:16:42 -0300

    Update bibliografia.R

--- R/bibliografia.R
+++ R/bibliografia.R
@@ -1,5 +1,5 @@
-gs4_deauth()
-bib=read_sheet("https://docs.google.com/spreadsheets/d/1LIjyLRj2Yr-wD8rwUQzgpA8PhQsLz2KyLEC9mLC9kKU/edit?usp=sharing")
+googlesheets4::gs4_deauth()
+bib=googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1LIjyLRj2Yr-wD8rwUQzgpA8PhQsLz2KyLEC9mLC9kKU/edit?usp=sharing")
 
 ref <- function(chave,cont,bibliografia){
 bibli = bib$Bibliografia[bib$ref==chave]
