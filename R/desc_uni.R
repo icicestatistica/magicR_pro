5731539b7dcd3839f7289cfd63ae5c4bfd48bcce
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-23 11:36:21 -0300

    Update desc_uni.R

--- R/desc_uni.R
+++ R/desc_uni.R
@@ -3,7 +3,7 @@ desc_uni_continua <- function(var,digitos){
   if(length(summary(var))==6) {N=length(var); na=0} else {N=length(var);na=summary(var)[7]}
   if(length(var)-sum(is.na(var))<3 | length(var)-sum(is.na(var))>3000 | min(var,na.rm=T)==max(var,na.rm=T)) p = "N/A" else p=pvalor(shapiro.test(var)$p.value)
   parametros <- c("N/A","Observações","Min-Máx","Q1-Q3","Mediana","Média","Desvio Padrão","CV", "Normalidade (Shapiro Wilk)")
-  if(n-na<2) sd="-" else sd=round(sd(var,na.rm=T),digitos)
+  if(N-na<2) sd="-" else sd=round(sd(var,na.rm=T),digitos)
   variavel <- c(paste0(na," (",100*round(na/N,digitos),"%)"),
                 N-na,
                 paste0(round(summary(var)[1],digitos),"-",round(summary(var)[6],digitos)),
