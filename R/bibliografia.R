7718dc2c61b7e94bd074451207169d78d856f4bf
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-23 01:41:31 -0300

    Create bibliografia.R

---
+++ R/bibliografia.R
@@ -0,0 +1,9 @@
+gs4_deauth()
+bib=read_sheet("https://docs.google.com/spreadsheets/d/1LIjyLRj2Yr-wD8rwUQzgpA8PhQsLz2KyLEC9mLC9kKU/edit#gid=0")
+
+ref <- function(chave,cont,bibliografia){
+bibli = bib$Bibliografia[bib$ref==chave]
+cont=cont+1
+marc = paste("[",cont,"]", sep="")
+bibliografia = c(bibliografia, paste(marc, " ",bibli, sep=""))
+return(list("marc"=marc,"cont"=cont,"bibliografia"=bibliografia))}
