f9aa2cd5065e5b91ccaa93b4da9dddcc90bdff2c
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-23 02:06:35 -0300

    Update metodologias.R

--- R/metodologias.R
+++ R/metodologias.R
@@ -1,4 +1,4 @@
-metodologias <- function(analises){
+metodologias <- function(analises, cont, bibliografia){
 
 n_analises = dim(analises)[1]
 matanalises = c()
@@ -8,7 +8,7 @@ matanalises <- data.frame("Nome"=analises$Nome,matanalises)
 
 totaltab = apply(matanalises[,2:6],2,sum)
   
-if(totaltab[1]>0) { ref1=ref("desc",cont,bibliografia); cont=ref1$cont ; bibliografia=ref1$bibliografia ;
+if(totaltab[1]>0) { ref1=ref("desc",cont,bibliografia);
 cat("**Estatísticas Descritivas:**
 
 *	N: Número de indivíduos em cada categoria
@@ -34,4 +34,5 @@ Adotaremos neste trabalho a significância de 5%. P-valores menores que 5% serã
 
 **Testes Estatísticos:**
 
-* **Shapiro-Wilk (SW):** Teste de normalidade realizado nos dados. A hipótese nula é que os dados apresentam distribuição normal e a hipótese alternativa, que não apresentam. Ao nível de significância de 5%, rejeita-se a hipótese de normalidade se o p-valor encontrado for menor que 0,05. Este teste é utilizado para determinar qual o teste estatístico adequado para ser utilizado nos dados. \n", sep="")}}
+* **Shapiro-Wilk (SW):** Teste de normalidade realizado nos dados. A hipótese nula é que os dados apresentam distribuição normal e a hipótese alternativa, que não apresentam. Ao nível de significância de 5%, rejeita-se a hipótese de normalidade se o p-valor encontrado for menor que 0,05. Este teste é utilizado para determinar qual o teste estatístico adequado para ser utilizado nos dados. \n", sep="")}
+return(list(cont=ref1$cont ; bibliografia=ref1$bibliografia))}
