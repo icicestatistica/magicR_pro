145835f8e03038a81886dbc4bab3eb4bbc827b7b
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-23 12:06:39 -0300

    Update desc_uni.R

--- R/desc_uni.R
+++ R/desc_uni.R
@@ -3,17 +3,17 @@ desc_uni_continua <- function(var,digitos){
   if(length(summary(var))==6) {N=length(var); na=0} else {N=length(var);na=summary(var)[7]}
   if(length(var)-sum(is.na(var))<3 | length(var)-sum(is.na(var))>3000 | min(var,na.rm=T)==max(var,na.rm=T)) p = "N/A" else p=pvalor(shapiro.test(var)$p.value)
   parametros <- c("N/A","Observações","Min-Máx","Q1-Q3","Mediana","Média","Desvio Padrão","CV", "Normalidade (Shapiro Wilk)")
-  if(sum(is.na(var))==length(var)) variavel=c(paste0(na," (100%)"),0,"-","-","-","-","-","-","-") else
+  if(sum(is.na(var))==length(var)) variavel=c(paste0(na," (100%)"),0,"-","-","-","-","-","-","-") else {
   variavel <- c(paste0(na," (",100*round(na/N,digitos),"%)"),
                 N-na,
                 paste0(round(summary(var)[1],digitos),"-",round(summary(var)[6],digitos)),
                 paste0(round(summary(var)[2],digitos),"-",round(summary(var)[5],digitos)),
                 round(summary(var)[3],digitos),
                 round(summary(var)[4],digitos),
-                sd,
+                round(sd(var,na.rm=T),digitos),
                 paste0(round(sd(var,na.rm=T)/summary(var)[4]*100,0),"%"),
-                p)
-  d <- data.frame("Característica"=parametros,"Estatística"=variavel)
+                p)}
+  d <- data.frame("Característica"=parametros,"Estatística"=unlist(variavel))
   return(d)}
 
 desc_uni_categorica <- function(var,niveis,nas,label,ordenar,acumula,digitos){
