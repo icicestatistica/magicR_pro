5ba9ad735c6514a8ff6b33452e98d0ca81069d6a
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-25 13:11:27 -0300

    Update fun_auxiliares.R

--- R/fun_auxiliares.R
+++ R/fun_auxiliares.R
@@ -43,5 +43,7 @@ if(class(a[[i]])=="data.frame") print(kable(a[[i]], row.names=F)) else
 
    
 cont_analises <- function(nome,vetortestes){
-df = data.frame("Nome"=nome,"analises"=paste("list('desc'=",vetortestes[1],", 'diffreq'=",vetortestes[2],", 't'=",vetortestes[3],", 'mw'=",vetortestes[4],", 'cc'=",vetortestes[5],")"))
+df = data.frame("Nome"=nome,"analises"=paste("list(c('desc'=",vetortestes[1],",'catsame'=",vetortestes[2],",'t'=",vetortestes[3],
+",'mw'=",vetortestes[4],",'aov1'=",vetortestes[5],",'kw'=",vetortestes[6],",'correl'=",vetortestes[7],",'cc'=",vetortestes[8],
+",'t_par'=",vetortestes[9],",'wilc'=",vetortestes[10],",'aovmr'=",vetortestes[11],",'fried'=",vetortestes[12],",'mcnem'=",vetortestes[13],",'qcoch'=",vetortestes[14],"))"))
 return(df)}
