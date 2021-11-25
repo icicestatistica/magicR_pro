27eaaea2cb0abd95ac195c94698ed6c73970ead9
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-24 21:06:53 -0300

    Delete bibliografia.R

--- R/bibliografia.R
+++
@@ -1,9 +0,0 @@
-googlesheets4::gs4_deauth()
-bib=googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1LIjyLRj2Yr-wD8rwUQzgpA8PhQsLz2KyLEC9mLC9kKU/edit?usp=sharing")
-
-ref <- function(chave,cont,bibliografia){
-bibli = bib$Bibliografia[bib$ref==chave]
-cont=cont+1
-marc = paste("[",cont,"]", sep="")
-bibliografia = c(bibliografia, paste(marc, " ",bibli, sep=""))
-return(list("marc"=marc,"cont"=cont,"bibliografia"=bibliografia))}
