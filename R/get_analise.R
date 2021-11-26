a057ad2536ea0d847a50309b0dbf58475cadbf68
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-26 19:21:46 -0300

    Update get_analise.R

--- R/get_analise.R
+++ R/get_analise.R
@@ -1,23 +1,22 @@
 escolhateste <- function(x,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,excluirtotal){
-  result<-data.frame()
-  tex=""
-  t=0
+  result<-data.frame() ; tex=""
+  t=0 ; mw=0
   if(tipox=="numeric") {
     if(tipoy=="factor") {
-      if(length(niveisy)==2)
-        {res = testet(x,y,nomex,nomey,niveisy,dig,F,excluirtotal)
-         if(res$sup==T) { result=res$resul
-                          tex=res$texto
-                          t=1}}
-    }} else
-  {if(tipox=="factor")
-    if(tipoy=="numeric"){
-      if(length(niveisx)==2)
-        {res = testet(y,x,nomey,nomex,niveisx,dig,T,excluirtotal)
-         if(res$sup==T) { result=res$resul
-                          tex=res$texto
-                          t=1}}}
-  }
+      if(length(niveisy)==2) {
+         res = testet(x,y,nomex,nomey,niveisy,dig,F,excluirtotal)
+         if(res$sup==F) {res=mann(x,y,nomex,nomey,niveisy,dig,F,excluirtotal) ; mw=1} else {t=1}
+         result=res$resul
+         tex=res$texto}
+  }} else {
+  if(tipox=="factor") {
+    if(tipoy=="numeric") {
+      if(length(niveisx)==2) {
+        res = testet(y,x,nomey,nomex,niveisx,dig,T,excluirtotal)
+        if(res$sup==F) {res=mann(x,y,nomex,nomey,niveisy,dig,F,excluirtotal) ; mw=1} else {t=1}
+                        result=res$resul
+                        tex=res$texto}
+  }}
   return(list("testes"=c("desc"=0,"catsame"=0,"t"=t,"mw"=0,"aov1"=0,"kw"=0,"correl"=0,"cc"=0,"t_par"=0,"wilc"=0,"aovmr"=0,"fried"=0,"mcnem"=0,"qcoch"=0),
               "result"=result,
               "texto"=tex))}
