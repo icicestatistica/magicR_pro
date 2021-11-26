b9ad612e2afa31457b59ff6f469339c7cff2ccb5
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-26 15:06:57 -0300

    Update testet.R

--- R/testet.R
+++ R/testet.R
@@ -45,7 +45,7 @@ if (teste$p.value > 0.05) texto=c(" * **",ref,":** Realizando o teste-t bicaudal
 
 
 res = desc_bi_cont(d$resp,d$fator,F,respcol,F,dig)
-res <- cbind(res,"p-valor"=c(p,rep("",dim(res)[1]-2)))
+res <- cbind(res,"p-valor"=c(p,rep("",dim(res)[1]-1)))
 
 return(list("testes"=c("desc"=0,"catsame"=0,"t"=1,"mw"=0,"aov1"=0,"kw"=0,"correl"=0,"cc"=0,"t_par"=0,"wilc"=0,"aovmr"=0,"fried"=0,"mcnem"=0,"qcoch"=0),
             "result"=res,
