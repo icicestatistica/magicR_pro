b8e4bb392d40dc5562cd73ff78f880c9927f7421
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-11-30 12:20:34 -0300

    Update testet.R

--- R/testet.R
+++ R/testet.R
@@ -45,7 +45,7 @@ if (teste$p.value > 0.05) texto=c(" * **",ref,":** Realizando o teste-t bicaudal
                                  " ).",mag," Apesar disso, cabe considerar a relevância clínica ou prática da diferença. ",sup,"\n")
 
 res=desc_bi_cont(d$resp,d$fator,F,respcol,F,dig)
-tot=res[3,2]
+tot=dim(na.omit(d$y,d$x)[1]
   
 if(excluirtotal==T) res=res[-1,]
   
