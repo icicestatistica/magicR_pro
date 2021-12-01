3daa1a26d4db11be2b81a28df3016c1df47df953
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-12-01 11:28:48 -0300

    Update get_analise.R

--- R/get_analise.R
+++ R/get_analise.R
@@ -39,7 +39,7 @@ get_analise_2 <- function(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,exc
   testes=c("desc"=0,"catsame"=0,"t"=0,"mw"=0,"aov1"=0,"kw"=0,"correl"=0,"cc"=0,"t_par"=0,"wilc"=0,"aovmr"=0,"fried"=0,"mcnem"=0,"qcoch"=0)
   result=data.frame()
   for (i in 1:xdim)
-    {res = escolhateste(xmat[,i],y,tipox[i],tipoy,nomex[i],nomey,eval(parse(text=niveisx[i])),eval(parse(text=niveisy)),dig,excluirtotal)
+    {res = escolhateste(xmat[,i],y,tipox[i],tipoy,nomex[i],nomey,eval(parse(text=niveisx[i])),eval(parse(text=niveisy)),dig,excluirtotal,respcol)
      result= rbind(result, res$result)
      texto <- c(texto,res$texto)
      testes <- testes+res$testes}
