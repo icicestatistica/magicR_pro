8b06ff0142c423a3fadfb6b0be2c99e6c4860d43
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2021-12-01 11:25:33 -0300

    Update get_analise.R

--- R/get_analise.R
+++ R/get_analise.R
@@ -33,7 +33,7 @@ escolhateste <- function(x,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,excluir
               "result"=result,
               "texto"=tex))}
 
-get_analise_2 <- function(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,excluirtotal){
+get_analise_2 <- function(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,excluirtotal,respcol){
   xdim=dim(xmat)[2]
   texto=c()
   testes=c("desc"=0,"catsame"=0,"t"=0,"mw"=0,"aov1"=0,"kw"=0,"correl"=0,"cc"=0,"t_par"=0,"wilc"=0,"aovmr"=0,"fried"=0,"mcnem"=0,"qcoch"=0)
@@ -46,10 +46,10 @@ get_analise_2 <- function(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,exc
 return(list("testes"=testes,"result"=result,"texto"=texto))}
 
 
-get_analise <- function(dados,col,auxiliar,y,nomey,niveisy,dig,excluirtotal){
+get_analise <- function(dados,col,auxiliar,y,nomey,niveisy,dig,excluirtotal,respcol){
   xmat=dados[,col]
   tipox=auxiliar$tipo[col]
   nomex=auxiliar$nomes[col]
   niveisx=auxiliar$niveis[col]
-  res = get_analise_2(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,excluirtotal)
+  res = get_analise_2(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,excluirtotal,respcol)
 return(list("testes"=res$testes,"result"=res$result,"texto"=res$texto))}
