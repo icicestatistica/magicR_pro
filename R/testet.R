c285e6c2ece82ab7042967a92dcb1dac7b3c9083
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-01-26 11:44:34 -0300

    Update testet.R

--- R/testet.R
+++ R/testet.R
@@ -21,14 +21,14 @@ if(str1$p.value<0.05 & str2$p.value>=0.05) sup=c(" A suposição de normalidade 
 if(str1$p.value<0.05 & str2$p.value<0.05) sup=c(" A suposição de normalidade das amostras foi verificada através do teste de Shapiro-Wilk, que com p-valor menor que 0.05 rejeitou a normalidade da distribuição dos dois grupos (",niveis[1]," - W=",round(str1$statistic,dig),", p-valor=",pvalor(str1$p.value),",  ",niveis[2]," - W=",round(str2$statistic,dig),", p-valor=",pvalor(str2$p.value),"). Com a suposição de normalidade violada, sugerimos a realização do teste não paramétrico Mann-Whitney ao invés do teste-t.")}
 
 ef= round(cohens_d(resp ~ fator, var.equal=F,data=d)$effsize,dig)
-if(is.na(ef)==T) {ef="-" ; mag="Não é possível calcular a estatística d de cohen com este tamanho de amostra."} else {
+if(is.na(ef)==T) {p="-" ; texto=c(" * **",ref,":** Não é possível realizar o teste estatístico com este tamanho de amostra. \n")} else {
 if (ef<0) dif="menor que" else dif="maior que"
 
 if (abs(ef)<0.2) mag=paste0(" Através da estatística d de cohen (",ef,"), verificamos a magnitude da diferença entre as médias menor que 0.2 DP, o que Cohen (1988) considerou desprezível, mesmo que seja estatisticamente significativa.") else
   if(abs(ef)<0.4) mag=paste0(" Através da estatística d de cohen (",ef,"), verificamos a magnitude da diferença entre as médias entre 0.2 e 0.4 DP's, o que Cohen (1988) considerou uma magnitude pequena.") else
     if(abs(ef)<0.8) mag=paste0(" Através da estatística d de cohen (",ef,"), verificamos a magnitude da diferença entre as médias entre 0.3 e 0.8 DP's, o que Cohen (1988) considerou uma magnitude média.") else
       mag=paste0(" Através da estatística d de cohen (",ef,"), verificamos a magnitude da diferença entre as médias maior que 0.8 DP's, o que Cohen (1988) considerou uma magnitude grande.")
-}
+
    
 teste=t.test(resp ~ fator,data=d,var.equal=F)
 
@@ -44,7 +44,7 @@ if (teste$p.value > 0.05) texto=c(" * **",ref,":** Realizando o teste-t bicaudal
 ", DP=",round(sd(resp1, na.rm=T),dig), ") apresenta média ",dif," o grupo '",niveis[2],"' (Média=",round(mean(resp2, na.rm=T),dig),", DP=",round(sd(resp2, na.rm=T),dig),"). A estimativa de diferença entre as médias foi ",
                                   round(teste$estimate[1]-teste$estimate[2],dig)," e IC95%=(",round(teste$conf.int[1],dig),", ",round(teste$conf.int[2],2),
                                  " ).",mag," Apesar disso, cabe considerar a relevância clínica ou prática da diferença. ",sup,"\n")
-}
+}}
 res=desc_bi_cont(d$resp,d$fator,F,respcol,F,dig)
 tot=dim(na.omit(d))[1]
   
