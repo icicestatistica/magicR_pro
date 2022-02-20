881a7e02fd80fd162768e6eace22c817c59f96b5
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-02-19 21:13:17 -0300

    Update format_pvalor.R

--- R/format_pvalor.R
+++ R/format_pvalor.R
@@ -1,8 +1,8 @@
 pvalor <- function(p){
   if(is.numeric(p)==F) res="" else
-    if(p>0.05) res = p=formatC(round(p,3), format='f', digits=3) else
-      if(p>0.01) res=paste0(formatC(round(p,3), format='f', digits=3),"\\*") else
-        if(p>=0.001) res=paste0(formatC(round(p,3), format='f', digits=3),"\\*\\*") else res = "<0.001\\*\\*\\*"
+    if(p>0.05) res = p=paste0("p=",formatC(round(p,3), format='f', digits=3) else
+      if(p>0.01) res=paste0("p=",formatC(round(p,3), format='f', digits=3),"\\*") else
+        if(p>=0.001) res=paste0("p=",formatC(round(p,3), format='f', digits=3),"\\*\\*") else res = "p<0.001\\*\\*\\*"
       return(res)}
 
 pvetor <- function(vetor){
