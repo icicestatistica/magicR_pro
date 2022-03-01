26616f5e3be3d4d3d79e545750b3c4acddd5413f
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-03-01 15:49:03 -0300

    Update get_summary.R

--- R/get_summary.R
+++ R/get_summary.R
@@ -1,4 +1,4 @@
-escolha_summary_para_juntar <- function(x,nomesx,tipox,niveisx,nas,teste,grafico,cor,dig){
+escolha_summary_para_juntar <- function(x,nomesx,tipox,niveisx,nas,teste,grafico,cor,bins,dig){
 if (tipox=="factor") {resulta=desc_uni_categorica(x,nomesx,eval(parse(text=niveisx)),nas,T,T,F,teste,grafico,cor,dig)
                       result=resulta$result[,c(1,4)]
                       texto=resulta$texto
@@ -9,26 +9,26 @@ if (tipox=="factor") {resulta=desc_uni_categorica(x,nomesx,eval(parse(text=nivei
                         texto=resulta$texto
                         tabela=resulta$tabela
                         grafico=resulta$grafico} else
-    {result=desc_uni_continua(x,dig)
+    {resulta=desc_uni_continua(x,nomesx,bins,teste,grafico,cor,digitos)
      texto=resulta$texto
-     tabela=resulta$tabela
+     tabela=NULL
      grafico=resulta$grafico}
 result=data.frame(result)
 result = cbind(c(nomesx,rep("",dim(result)[1]-1)),result)
 names(result) = c("Variável","Característica","Estatística")
 return(list("result"=result,"texto"=texto,"tabela"=tabela,"grafico"=grafico))}
 
-get_summary_2 <- function(x,nomesx,tipox,niveisx,nas,teste,grafico,cor,dig){
+get_summary_2 <- function(x,nomesx,tipox,niveisx,nas,teste,grafico,cor,bins,dig){
   cont=0
   xdim <- dim(x)[2]
   cont=cont+1
-  resulta <- escolha_summary_para_juntar(x[,1],nomesx[1],tipox[1],niveisx[1],nas,teste,grafico,cor,dig)
+  resulta <- escolha_summary_para_juntar(x[,1],nomesx[1],tipox[1],niveisx[1],nas,teste,grafico,cor,bins,dig)
   result=resulta$result
   complem=list("texto"=resulta$texto,"tabela"=resulta$tabela,"grafico"=resulta$grafico,"\n")
   if (xdim>1){
   for (i in 2:xdim){
     cont=cont+1
-    resulta=escolha_summary_para_juntar(x[,i],nomesx[i],tipox[i],niveisx[i],nas,teste,grafico,cor,dig)
+    resulta=escolha_summary_para_juntar(x[,i],nomesx[i],tipox[i],niveisx[i],nas,teste,grafico,cor,bins,dig)
     result <- rbind(result,resulta$result)
     complem <- list.append(complem,resulta$texto,resulta$tabela,resulta$grafico,"\n")}}
 row.names(result) <- 1:dim(result)[1]
@@ -36,10 +36,10 @@ return(list("testes"=c("desc"=cont,"catsame"=0,"t"=0,"mw"=0,"aov1"=0,"kw"=0,"cor
             "result"=result,
             "complem"=complem))}
 
-get_summary <- function(dados,gr,auxiliar,nas,teste,grafico,cor,dig){
+get_summary <- function(dados,gr,auxiliar,nas,teste,grafico,cor,bins,dig){
 x <- dados[,gr]
 nomesx <- auxiliar[gr,2]
 tipox <- auxiliar[gr,3]
 niveisx <- auxiliar[gr,4]
-resultados = get_summary_2(x,nomesx,tipox,niveisx,nas,teste,grafico,cor,dig)
+resultados = get_summary_2(x,nomesx,tipox,niveisx,nas,teste,grafico,cor,bins,dig)
 return(list("testes"=resultados$testes,"result"=resultados$result,"complem"=resultados$complem))}
