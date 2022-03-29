8347e10de6d14cd0b115fdb7f2158e80dbf65055
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-03-29 11:26:05 -0300

    Update contcont.R

--- R/contcont.R
+++ R/contcont.R
@@ -55,7 +55,8 @@ if(str1$p.value<0.05 & str2$p.value<0.05) sup=c(" A suposição de normalidade d
     if(a$p.value<0.05) {texto=c("* **",nomex,"**: O teste de correlação de ",method, " rejeitou a hipótese de nulidade de correlação (",greg,"=",
     round(a$estimate,dig),", p-valor=",pvalor(a$p.value),"), indicando uma relação ",direcao," ",mag,coefdet,ictexto,sup,"\n"); significativo=T} else {texto=c("* **",nomex,"**: Não encontramos evidências através do teste de correlação de ",method, " para rejeitar a hipótese de nulidade de correlação (",greg,"=",round(a$estimate,dig),", p-valor=",pvalor(a$p.value),"). De fato, ",mag,coefdet,ictexto,sup," \n"); significativo=F}
     
-    textograf=paste0(greg,"=", round(a$estimate,dig),", p-valor=",pvalor(a$p.value),collapse="")
+   rhoprint=round(rho,dig) ; pvalorprint= ifelse(a$p.value<0.001,"<0.001",round(a$p.value,3)) ; methodprint=method
+textograf=substitute(paste("Correlação de ",methodprint," (",rho,"=",rhoprint,", p-valor=",pvalorprint,")"), list(rhoprint=rhoprint,pvalorprint=pvalorprint,methodprint=method))
     
     result <- data.frame("Variável"=nomex,"p-valor"=p,"rho"=rho,"rho2"=rho2,"IC"=IC)
     names(result)=c("Variável","p-valor","Estatística","Variância compartilhada","IC (95%)")
