38e948cbbf4fdf9099614059ecaf72801a13e072
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-04-07 02:29:38 -0300

    Update get_analise.R

--- R/get_analise.R
+++ R/get_analise.R
@@ -9,7 +9,7 @@ escolhateste <- function(x,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,excluir
            result=res$resul
            tex=res$texto
            grafico=res$grafico} else
-                        {res = anovac(x,y,nomex,nomey,niveisy,dig,F,excluirtotal)
+                        {res = anovac(x,y,nomex,nomey,niveisy,dig,F,excluirtotal,cor)
                         if(res$sup==F) {res=kruskall(x,y,nomex,nomey,niveisy,dig,F,excluirtotal,cor);kw=1} else {aov1=1}
                         result=res$result
                         tex=res$texto
@@ -29,7 +29,7 @@ escolhateste <- function(x,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,excluir
                         result=res$resul
                         tex=res$texto
                         grafico=res$grafico} else
-                                          {res = anovac(y,x,nomey,nomex,niveisx,dig,T,excluirtotal)
+                                          {res = anovac(y,x,nomey,nomex,niveisx,dig,T,excluirtotal,cor)
                                            if(res$sup==F) {res=kruskall(y,x,nomey,nomex,niveisx,dig,T,excluirtotal,cor);kw=1} else {aov1=1}
                                            result=res$result
                                            tex=res$texto
