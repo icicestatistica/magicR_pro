eb1b40f6948e975c2b237cedf8e7296a74b7f210
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-04-14 16:50:38 -0300

    Update categorica_graficoeteste.R

--- R/categorica_graficoeteste.R
+++ R/categorica_graficoeteste.R
@@ -35,9 +35,12 @@ grafico_categorica <- function(var,nome, niveis, cor, ordenar){
         panel.grid.major = element_blank(), panel.grid.minor = element_blank())} else
               {result = ggplot(tab, aes(x="",y=Freq,fill=var)) +
               geom_bar(stat="identity", width=1) +
-              coord_polar("y", start=0) + theme_void() +
+              coord_polar("y", start=0) + theme_clean() +
               labs(fill="",title=paste0(vetor_comsep_c(nome,50)," (n=",length(var),")")) +
-              theme(plot.title = element_text(hjust = 0.5, size=12)) + 
+              theme(plot.title = element_text(hjust = 0.5, size=12),
+              plot.background = element_rect(colour="white"),
+              axis.text.x=element_text(size=12),
+              panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
               geom_text(aes(label = prop), color = "white", position = position_stack(vjust = 0.5)) +
               scale_fill_manual(labels = vetor_comsep_c(niveis,11),values=lighten(cor,seq(0,0.3,(0.3/(length(tab$var)-1)))))}}
   if(ordenar==F) {
