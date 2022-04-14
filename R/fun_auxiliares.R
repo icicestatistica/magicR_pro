5841b3223805830cd3878c517e058046973d1b85
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-04-14 05:51:24 -0300

    Update fun_auxiliares.R

--- R/fun_auxiliares.R
+++ R/fun_auxiliares.R
@@ -35,6 +35,29 @@ for (i in 1:length(vec)){
     aux <- c(aux, paste(mat[[i]][((linhas-1)*corte)+(1:ultima)],collapse=" ",sep=""))
   res=c(res, paste(aux, collapse=" \n "))}}
 return(res)}
+   
+vetor_comsep_c <- function(vec,corteinicial){
+  res=c()
+  for (vetor in vec){
+  a=T
+  espacos = c(str_locate_all(vetor," ")[[1]][,1],nchar(vetor)+1)
+  inicio=1
+  corte=corteinicial
+  fim= ifelse(espacos[1]>corte,espacos[1]-1,espacos[max(which(espacos-1<=corte))]-1)
+  newvec <- c()
+  while (a){
+    newvec = c(newvec,str_sub(vetor,inicio,fim))
+    inicio = fim + 2
+      if(inicio>nchar(vetor)) a=F
+    corte=inicio+corteinicial
+    fim=espacos[max(which(espacos-1<=corte))]-1
+    if(fim<inicio) {fim=espacos[max(which(espacos-1<=corte))+1]-1}
+  }
+  newvec = paste0(newvec, collapse="\n")
+  res = c(res,newvec)}
+return(res)}
+
+#vetor_comsep_c(names(dados),50)
 
 relatorio <- function(a,pularprimeiro){
 if (pularprimeiro==T) comeco=2 else comeco=1
