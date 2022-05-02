dd6265b7aa1aa1e9f3b575f7dbfbcef5191a0669
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-05-02 08:29:12 -0300

    Update wilcoxon.R

--- R/wilcoxon.R
+++ R/wilcoxon.R
@@ -1,13 +1,16 @@
-wilcox = function(vec1_emordem, vec2_emordem,niveisemordem, nomey="Escore"){
+wilcox = function(vec1_emordem, vec2_emordem,niveisemordem, nomey="Escore", idioma="PT"){
 
 t3 = wilcox.test(vec1_emordem,vec2_emordem, paired=T)
 
 d= data.frame("ID"=rep(1:length(vec1_emordem),times=2),"Tempo"=rep(niveisemordem, each=length(vec1_emordem)),"Escore"=c(vec1_emordem,vec2_emordem))
 d$Tempo = factor(d$Tempo, levels = niveisemordem)
 
+if(idioma=="PT") textos = c("Comparação de", "Teste de Wilcoxon (V=", " p-valor=") else 
+if(idioma=="EN") textos= c("Comparison of","Wilcoxon's Test (V="," p-value=")
+ 
 grafico = ggplot(d, aes(y=Escore, x=Tempo)) +  geom_point() + geom_line(aes(group=ID)) + theme_clean() + 
-labs(title=vetor_comsep_c(paste0("Comparação de ",printvetor(niveisemordem)," (n=",length(unique(d$ID)),")"),50), 
-subtitle=paste0("Teste de Wilcoxon (V=",t3$statistic,", p-valor=",ifelse(t3$p.value<0.001,"<0.001",round(t3$p.value,3)),")"), y=nomey) +
+labs(title=vetor_comsep_c(paste0(textos[1],printvetor(niveisemordem)," (n=",length(unique(d$ID)),")"),50), 
+subtitle=paste0(textos[2],t3$statistic,textos[3],ifelse(t3$p.value<0.001,"<0.001",round(t3$p.value,3)),")"), y=nomey) +
  theme(plot.title=element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5), axis.title.x=element_blank(), plot.background = element_rect(colour = "white")) +
  scale_y_continuous(limits=c(0,max(d$Escore)))
 
