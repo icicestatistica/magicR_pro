555ac64299905eed139698e6ec01bd1dc36a925f
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-06-15 11:00:32 -0300

    Update cat_same_levels.R

--- R/cat_same_levels.R
+++ R/cat_same_levels.R
@@ -1,4 +1,7 @@
-cat_same_levels <- function(x,nomes,levels,nas,dig){
+cat_same_levels <- function(x,nomes='auto',levels='auto',nas=F,dig=2){
+  
+if (nomes=='auto') nomes = names(x)
+if (levels=='auto') levels = names(table(x[,1])
 cont=1
 df = data.frame("Variável"=nomes[1],t(desc_uni_categorica(x[,1],"",levels,nas,T,F,F,F,F,F,dig)$result[,4]))
 if(dim(x)[2]>1){
@@ -8,9 +11,10 @@ names(df)=c("Variável",levels)
 return(list("testes"=c("desc"=0,"catsame"=cont,"t"=0,"mw"=0,"aov1"=0,"kw"=0,"correl"=0,"cc"=0,"t_par"=0,"wilc"=0,"aovmr"=0,"fried"=0,"mcnem"=0,"qcoch"=0),
             "result"=df))}
 
-cat_same_levels_2 <- function(x,nomes=" ",nomey,levels,dig=2,cor="darkblue",sepvetor=7){
+cat_same_levels_2 <- function(x,nomes="auto",nomey,levels="auto",dig=2,cor="darkblue",sepvetor=7){
 cont=1
-if(nomes==" ") nomes = names(x)
+if(nomes=="auto") nomes = names(x)
+if (levels=='auto') levels = names(table(x[,1])
 prop=prop.test(table(factor(unlist(x[,1]),levels=levels)))
 df = data.frame("Variável"=nomes[1],desc_uni_categorica(x[,1],"",levels,F,F,F,F,F,F,F,dig)$result[1,c(2,3)],prop$conf.int[1],prop$conf.int[2])
 for (i in 2:dim(x)[2]){
