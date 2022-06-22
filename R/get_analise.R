43adb41ffa0afc3ecda2d7863ee391ff49efdcd0
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-06-22 10:12:17 -0300

    Update get_analise.R

--- R/get_analise.R
+++ R/get_analise.R
@@ -6,7 +6,7 @@ escolhateste <- function(x,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig=2,exclu
     if(tipoy=="factor") {
       if(length(niveisy)==2) 
            {res = testet(as.numeric(unlist(x)),y,nomex,nomey,niveisy,dig,F,excluirtotal,cor)
-           if(res$sup==F | tipox=="ordinal") {res=mann(y,as.numeric(unlist(x)),nomey,nomex,niveisy,dig,F,excluirtotal,cor,idioma,transform_ord) ; mw=1} else {t=1}
+           if(res$sup==F | tipox=="ordinal") {res=mann(y,x,nomey,nomex,niveisy,dig,F,excluirtotal,cor,idioma,transform_ord) ; mw=1} else {t=1}
            result=res$resul
            tex=res$texto
            grafico=res$grafico} else
@@ -27,7 +27,7 @@ escolhateste <- function(x,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig=2,exclu
       transform_ord = ifelse(tipoy=="ordinal",T,F)
       if(length(niveisx)==2)
         {res = testet(as.numeric(unlist(y)),x,nomey,nomex,niveisx,dig,T,excluirtotal,cor)
-        if(res$sup==F | tipoy=="ordinal") {res=mann(x,as.numeric(unlist(y)),nomex,nomey,niveisx,dig,T,excluirtotal,cor,idioma,transform_ord) ; mw=1} else {t=1}
+        if(res$sup==F | tipoy=="ordinal") {res=mann(x,y,nomex,nomey,niveisx,dig,T,excluirtotal,cor,idioma,transform_ord) ; mw=1} else {t=1}
                         result=res$resul
                         tex=res$texto
                         grafico=res$grafico} else
