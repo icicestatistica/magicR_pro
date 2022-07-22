bffc592e4377af6f79993ed7703d2aa0f8be06f2
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-07-22 10:45:34 -0300

    Update cat_same_levels.R

--- R/cat_same_levels.R
+++ R/cat_same_levels.R
@@ -1,4 +1,4 @@
-cat_same_levels <- function(dad,nomes='auto',niveis='auto',nas=F,dig=2,cor="cyan4",invertercores=F){
+cat_same_levels <- function(dad,nomes='auto',niveis='auto',nas=F,dig=2,cor="cyan4",invertercores=F, idioma="PT"){
   
 if(nomes[1]=='auto') nomes=names(dad)
 if(niveis=="auto") niveis=names(table(matrix(unlist(dad))))
@@ -31,7 +31,7 @@ plot=ggplot(newmat, aes(y=Coluna, x=Freq/n, fill=Resultado)) + geom_bar(stat="id
 return(list("testes"=c("desc"=0,"catsame"=cont,"t"=0,"mw"=0,"aov1"=0,"kw"=0,"correl"=0,"cc"=0,"t_par"=0,"wilc"=0,"aovmr"=0,"fried"=0,"mcnem"=0,"qcoch"=0),
             "t1"="A análise detalhada das variáveis está na tabela a seguir: \n","result"=df,"t2"="\n Podemos visualizar esses resultados no seguinte gráfico: \n","grafico"=plot))}
 
-cat_same_levels_2 <- function(x,nomes="auto",nomey,levels="auto",dig=2,cor="cyan4",sepvetor=7){
+cat_same_levels_2 <- function(x,nomes="auto",nomey,levels="auto",dig=2,cor="cyan4",sepvetor=7,idioma="PT"){
 cont=1
 if(nomes=="auto") nomes = names(x)
 if (levels=='auto') levels = c("Sim","Não")
@@ -58,10 +58,11 @@ texto=list.append(texto, paste("É possível visualizar esses resultados no grá
 
 df$Variável <- vetor_comsep(unlist(df$Variável),sepvetor)
 
+if(idioma=="PT") tituloeixox="Proporção em %" else tituloeixox="Proportion in %"
 result <- mutate(df, Variável=fct_reorder(Variável, desc(-as.numeric(str_sub(`Freq. Relativa`,end=-2)))))
 grafico=ggplot(result, aes(y = Variável, x = as.numeric(str_sub(`Freq. Relativa`,end=-2))/100)) +
   geom_bar(stat="identity", fill=cor) +
-  geom_errorbar(aes(xmax = ICmax, xmin = ICmin)) + xlab("Proporção em %") + ggtitle(nomey) + ylab("") + theme_minimal() + 
+  geom_errorbar(aes(xmax = ICmax, xmin = ICmin)) + xlab(tituloeixox) + ggtitle(nomey) + ylab("") + theme_minimal() + 
   scale_x_continuous(labels = scales::percent) + theme(plot.title = element_text(hjust = 0.5))
 
 return(list("testes"=c("desc"=0,"catsame"=cont,"t"=0,"mw"=0,"aov1"=0,"kw"=0,"correl"=0,"cc"=0,"t_par"=0,"wilc"=0,"aovmr"=0,"fried"=0,"mcnem"=0,"qcoch"=0),
