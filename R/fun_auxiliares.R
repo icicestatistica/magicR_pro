c28d5865d97d92b8a83f06d96ca183d2b2d03a01
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-08-07 10:46:21 -0300

    Update fun_auxiliares.R

--- R/fun_auxiliares.R
+++ R/fun_auxiliares.R
@@ -73,11 +73,11 @@ return(res)}
 relatorio <- function(a,pularprimeiro=T){
 if (pularprimeiro==T) comeco=2 else comeco=1
 for (i in comeco:length(a)){
-   if(is.null(a[[[i]]])==T) tantofaz=0 else {
-if(class(a[[[i]]])=="data.frame") print(kable(a[[[i]]], row.names=F)) else
-  if(class(a[[[i]]])=="list") relatorio(a[[i]],F) else
-      if(class(a[[[i]]])=="character") cat(a[[i]], sep="\n") else
-        print(a[[[i]]])}}}
+   if(is.null(a[[i]])==T) tantofaz=0 else {
+if(class(a[[i]])[1]=="data.frame") print(kable(a[[i]], row.names=F)) else
+  if(class(a[[i]])[1]=="list") relatorio(a[[i]],F) else
+      if(class(a[[i]])[1]=="character") cat(a[[i]], sep="\n") else
+        print(a[[i]])}}}
 
    
 cont_analises <- function(nome,vetortestes,vars){
