7a9e5a35307d5687e07b759fa49529d55d95f5d0
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-08-09 11:11:26 -0300

    Update fun_auxiliares.R

--- R/fun_auxiliares.R
+++ R/fun_auxiliares.R
@@ -93,10 +93,8 @@ if(class(a[[i]])[1]=="data.frame") print(kable(a[[i]], row.names=F)) else
       if(class(a[[i]])[1]=="character") cat(a[[i]], sep="\n") else
         print(a[[i]])}}}
    
-cont_analises <- function(nome,vetortestes,vars){
-df = data.frame("Nome"=nome,"analises"=paste("list(c('desc'=",vetortestes[1],",'catsame'=",vetortestes[2],",'t'=",vetortestes[3],
-",'mw'=",vetortestes[4],",'aov1'=",vetortestes[5],",'kw'=",vetortestes[6],",'correl'=",vetortestes[7],",'cc'=",vetortestes[8],
-",'t_par'=",vetortestes[9],",'wilc'=",vetortestes[10],",'aovmr'=",vetortestes[11],",'fried'=",vetortestes[12],",'mcnem'=",vetortestes[13],",'qcoch'=",vetortestes[14],"))"),Variáveis = printvetor(vars))
+cont_analises <- function(nome,a){
+df = data.frame("Sessão"=rep(nome,dim(a$testes)[1]),a$testes)
 return(df)}
    
 transpordf <- function(tab){
