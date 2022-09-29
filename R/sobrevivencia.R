86df7b5421d80282f63d8e380f14e11443c8f3d2
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-09-29 11:58:33 -0300

    Update sobrevivencia.R

--- R/sobrevivencia.R
+++ R/sobrevivencia.R
@@ -1,4 +1,4 @@
-
+sobrevivencia = function(wpm){
 df = na.omit(tidyr::pivot_longer(data=wpm, cols=c("t30","t60","t90"),names_to = "Tempo",values_to = "Ocorrência"))
 
 library(survival)
@@ -7,3 +7,4 @@ library(survminer)
 ggsurv = survminer::ggsurvplot(m, data = df, conf.int = T,pval = T,xlab = "Tempo em dias",   # customize X axis label.
            break.time.by = 30,palette = c("#E7B800", "#2E9FDF"))
 ggsurv$plot + labs(y="Percentual sem brotação", title="Análise de sobrevivência: Comparando o tempo \n até brotação nos grupos") + theme(plot.title=element_text(hjust=0.5))
+}
