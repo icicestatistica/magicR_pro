c070740f89ce62b11fe175ff2644a2459a8d45ae
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-10-05 10:10:53 -0300

    Update logistica.R

--- R/logistica.R
+++ R/logistica.R
@@ -1,9 +1,10 @@
 logisti = function(st){
 
 a=jtools::summ(st, exp = T)
-
+  
 res=data.frame(round(summary(st)$coefficients[,1],3),round(a$coeftable[,1],3),paste0("(",round(a$coeftable[,2],3),", ",round(a$coeftable[,3],3),")"),round(a$coeftable[,4],3),pvetor(a$coeftable[,5]))
-names(res)=c("Coeficiente (B)","OR","IC95%","Valor z","p-valor")
+res = data.frame(c("Intercepto",row.names(res)[-1]),res)
+names(res)=c("Fonte","Coeficiente (B)","OR","IC95%","Valor z","p-valor")
 
 library("sjPlot")
 
