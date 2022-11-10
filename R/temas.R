6fb8ed5a06248731d062e1b80b853247196e852d
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2022-11-10 14:25:33 -0300

    Update and rename themas.R to temas.R

--- R/temas.R
+++ R/temas.R
@@ -1,6 +1,7 @@
-theme_icic = ggthemes::theme_clean() + ggplot2::theme(plot.background = element_rect(colour = NA), 
+theme_icic = function(){
+return(ggthemes::theme_clean() + ggplot2::theme(plot.background = element_rect(colour = NA), 
         panel.background = element_rect(fill = "transparent", 
             color = NA), plot.title = element_text(hjust = 0.5), 
         plot.subtitle = element_text(hjust = 0.5), legend.background = element_rect(color = NA), 
         panel.grid.major.x = element_line(linetype = 3, color = "gray"), 
-        panel.grid.major.y = element_blank())
+        panel.grid.major.y = element_blank()))}
