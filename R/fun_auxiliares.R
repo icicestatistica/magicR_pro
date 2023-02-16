be510f2e0f5c921da93d009fec709b535e517e3f
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2023-02-15 21:17:43 -0300

    Update fun_auxiliares.R

--- R/fun_auxiliares.R
+++ R/fun_auxiliares.R
@@ -14,7 +14,7 @@ pvetor <- function(vetor){
 
 printvetor <- function(vetor, idioma="PT",aspas=T){
 size=length(vetor)
-if(aspas=T) {
+if(aspas==T) {
   conec = ifelse(idioma=="PT","' e '","' and '")
   if(size==1) print=paste0("'",vetor[1],"'") else print=paste(paste("'",vetor[1:(size-1)],collapse="', ",sep=""),conec,vetor[size],"'",collapse=NULL,sep="")} else {
     conec = ifelse(idioma=="PT"," e "," and ")
