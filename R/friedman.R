567f246398211119e3791c9afb1ff02f9dc88559
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2023-02-16 08:35:35 -0300

    Update friedman.R

--- R/friedman.R
+++ R/friedman.R
@@ -36,7 +36,7 @@ grafico = ggpubr::ggboxplot(df_long, x = "time", y = "num") +
   labs(
     subtitle = textograf,
     caption = get_pwc_label(pwc),
-    y=nomex)
+    y=nomex, x="Momento") + theme_icic("h")
 
 res=desc_bi_cont(df_long$num,df_long$time,respcol=F)[-1,-2]
 
