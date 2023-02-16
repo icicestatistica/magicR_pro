9473f2ca3bbb0bd7aec912f865bb03884bbef981
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2023-02-16 09:43:19 -0300

    Update wilcoxon.R

--- R/wilcoxon.R
+++ R/wilcoxon.R
@@ -1,7 +1,9 @@
-wilcox = function(id,time,num,nomex,moms){
+wilcox = function(id,time,num,nomex,moms,tipox="numeric"){
 
 id=factor(id)
 time=factor(time,levels=moms)
+  
+if(tipox=="ordinal") {num = as.numeric(num)}
 
 df=data.frame(id,time,num)
 df = df %>% filter(time %in% moms)
@@ -27,6 +29,7 @@ dif = ifelse(stat.test$p<0.05," foi estatisticamente diferente"," não foi estat
 
 texto = paste(" - A variável '", nomex, "'",dif," nos momentos ao nível de 5% de significância  usando o teste de Wilcoxon (V=", a2, ",p", a3, "), com effect size =", round(unname(es$effsize), 2), ", que pode ser considerado um efeito ", ifelse(es$magnitude == "small", "pequeno", ifelse(es$magnitude ==  "moderate", "moderado", "grande")), ".", sep = "")
     
+  if(tipox=="numeric"){
     grafico = ggpubr::ggboxplot(df_long, x = "time", y = "num") + 
     labs(subtitle = get_test_label(stat.test, detailed= TRUE), 
     y = nomex, x="Momento") + theme_icic("h") + ylim(0,max(df_long$num,na.rm=T))
@@ -34,7 +37,18 @@ texto = paste(" - A variável '", nomex, "'",dif," nos momentos ao nível de 5% 
     res = desc_bi_cont(df_long$num, df_long$time, respcol = F)[-1, 
         -2]
     res <- cbind(rbind(c(paste("**", nomex, "** (", dim(df_wide)[1], 
-        ")", sep = ""), rep("", dim(res)[2])), res), `p-valor` = c("", 
-        paste0(pvalor(stat.test$p), "j"), rep("", dim(res)[1] - 1)))
+        ")", sep = ""), rep("", dim(res)[2]-1)), res), `p-valor` = c("", 
+        paste0(pvalor(stat.test$p), "j"), rep("", dim(res)[1] - 1))) } else
+    
+    {
+    textograf <- substitute(paste("Teste de Wilcoxon (V=", a2, ",p", a3, ")", collapse = ""), list(a2 = a2, a3 = a3))
+    grafico = grafico_catcat(df_long$time, "Momento", df_long$num, 
+        nomex, texto = textograf) + labs(title = vetor_comsep_c(paste0("Diferença de proporções de ", 
+        nomex, " nos momentos"), 40))
+    res = desc_bi_cat(df_long$num, col = df_long$time, )[-1, 
+        -2]
+    res <- cbind(rbind(c(paste("**", nomex, "** (", dim(df_wide)[1], 
+        ")", sep = ""), rep("", dim(res)[2]-1)), res), `p-valor` = c("", 
+        paste0(pvalor(stat.test$p), "j"), rep("", dim(res)[1] - 1))) }
     
     return(list(result = res, grafico = grafico, texto = texto))}
