2bfcad4096886eda25f4d88d451a6737b698644e
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2023-02-17 11:04:29 -0300

    Update confiabilidade.R

--- R/confiabilidade.R
+++ R/confiabilidade.R
@@ -6,4 +6,15 @@ library("irr")
 #Cronbach
 
 #library("ltm")
-#cronbach.alpha(dados[,27:36], standardized = FALSE, CI = TRUE, probs = c(0.025, 0.975), B = 1000, na.rm = FALSE)
+cronbach_icic = function(dad,nomescala){
+
+a=cronbach.alpha(dad, standardized = FALSE, CI = TRUE, 
+    probs = c(0.025, 0.975), B = 1000, na.rm = FALSE)
+
+interp = cut(a$alpha,c(-0.1,0.21,0.41,0.61,0.8,1),labels=c("pequena","razoável","moderada","substancial","quase perfeita"))
+
+texto=paste0("Calculamos o alpha de cronbach para a escala ",nomescala,", com ",a$p," itens aplicados em ",a$n," indivíduos: alpha=",round(a$alpha,3),", IC95% ",paste0("[",paste0(round(a$ci,3),collapse=", "),"]"),". Landis, J.R., Koch, G.G. (1997) sugerem a intepretação dessa medida como ",interp,". \n")
+
+result=data.frame("Escala"=nomescala,"n"=a$n,"itens"=a$p,"alpha"=round(a$alpha,3),"IC95"=paste0("[",paste0(round(a$ci,3),collapse=", "),"]"),"interpretação"=interp)
+
+return(list(result,texto))}
