3f1de4b29d7b9fc0e0cbaa33e804401ed8d9e5f6
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2023-02-17 11:56:13 -0300

    Update confiabilidade.R

--- R/confiabilidade.R
+++ R/confiabilidade.R
@@ -1,7 +1,13 @@
 #ICC
-library("irr")
-#a=icc(dados[,c(114,202)], model = "twoway", type = "agreement", unit = "single")
-#paste("F(",round(a$df1,0),",",round(a$df2,0),")=",round(a$Fvalue,2), ", p=",pvalor(a$p.value),", ICC=",round(a$value,3),", IC 95% = (",round(a$lbound,2),", ",round(a$ubound,2),")",sep="",collapse="")
+
+icc_icic = function(mat){
+nomesmat = names(mat)
+a=irr::icc(mat, model = "twoway", type = "agreement", unit = "single")
+sn=ifelse(a$p.value<0.05,"rejeitamos","não rejeitamos")
+interp = cut(a$value,c(0,0.4,0.6,0.75,1.1),right=F, labels=c("ruim","regular","bom","excelente"))
+texto=paste(" - Calculamos o ICC (F(",round(a$df1,0),",",round(a$df2,0),")=",round(a$Fvalue,2), ", ICC=",round(a$value,3),", IC 95% = [",round(a$lbound,2),", ",round(a$ubound,2),"]), que pode ser interpretado, como sugere Cicchetti (1994), como uma concordância ",interp," e com p=",pvalor(a$p.value)," ",sn," a nulidade de Índice de Correlação Intraclasse entre ",printvetor(nomesmat,aspas=F),". \n",sep="",collapse="")
+return(list("texto"=texto))}
+
 
 #Cronbach
 
