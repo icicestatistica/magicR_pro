0b880ff0497518046faeb55b379913790f1fe8d4
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2023-02-23 10:42:12 -0300

    Update matrizcorrel.R

--- R/matrizcorrel.R
+++ R/matrizcorrel.R
@@ -12,5 +12,10 @@ for (i in 1:n)
       if (i<j) sig[i,j]="" else
           if(a$p[i,j]<0.01) sig[i,j]=paste0(round(m[i,j],3),"**") else if(a$p[i,j]<0.05) sig[i,j]=paste0(round(m[i,j],3),"*") else sig[i,j]=paste0(round(m[i,j],3))
 
-result=kable(data.frame(nomes,sig),col.names=c("",nomes), caption=paste0("Análises de correlação de ",metodo," entre ",printvetor(nomes)))
-return(result)}
+result=data.frame(nomes,sig)
+testes = data.frame(Nome1 = "", Nome2 = printvetor(nomes, aspas=F),tipo = "matcorrel", sig_ou_não = NA, resumo = NA, sup = NA)
+return(list("testes"=testes,"result"=result))}
+        
+matcorrel_meta = function(){
+  texto="- **Matriz de correlação**: Uma matriz de correlação consiste em linhas e colunas que mostram as correlações entre duas variáveis de cada vez, sendo uma especificada na linha e uma na coluna, de modo que cada célula da tabela contém o coeficiente de correlação. É uma ferramenta poderosa para resumir um grande conjunto de dados e para identificar e visualizar padrões nos dados fornecidos. \n"
+  }
