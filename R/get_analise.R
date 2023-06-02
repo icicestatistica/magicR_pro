630e9c13c9387f9e9cf61a1f56f86ec8fd1dcd39
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2023-06-02 06:06:57 -0300

    Update get_analise.R

--- R/get_analise.R
+++ R/get_analise.R
@@ -1,37 +1,37 @@
-escolhateste <- function(x,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig=2,excluirtotal=T,respcol=T,cor="cyan4",idioma="PT"){
+escolhateste <- function(x,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig=2,excluirtotal=T,respcol=T,cor="cyan4",idioma="PT",virgula=F){
   testes=data.frame() ; result<-data.frame() ; tex="" ; grafico=NULL
   if(tipox=="numeric" | tipox=="ordinal")
     {transform_ord = ifelse(tipox=="ordinal",T,F)
     if(tipoy=="factor")
       {if(length(niveisy)==2)
-        {res = try(testet(as.numeric(unlist(x)),y,nomex,nomey,niveisy,dig,F,excluirtotal,cor))
-          if(class(res)=="try-error") {res=mann(y,x,nomey,nomex,niveisy,dig,F,excluirtotal,cor,idioma,transform_ord,niveis_ord=niveisx)} else
-            if(res$testes$sup==F | tipox=="ordinal") {res=mann(y,x,nomey,nomex,niveisy,dig,F,excluirtotal,cor,idioma,transform_ord,niveis_ord=niveisx)}
+        {res = try(testet(as.numeric(unlist(x)),y,nomex,nomey,niveisy,dig,F,excluirtotal,cor,virgula))
+          if(class(res)=="try-error") {res=mann(y,x,nomey,nomex,niveisy,dig,F,excluirtotal,cor,idioma,transform_ord,niveis_ord=niveisx,virgula=virgula)} else
+            if(res$testes$sup==F | tipox=="ordinal") {res=mann(y,x,nomey,nomex,niveisy,dig,F,excluirtotal,cor,idioma,transform_ord,niveis_ord=niveisx,virgula=virgula)}
         } else
-          {res = try(anovac(unlist(x),y,nomex,nomey,niveisy,dig,F,excluirtotal,cor,idioma))
-            if(class(res)=="try-error") {res=kruskall(unlist(x),y,nomex,nomey,niveisy,dig,F,excluirtotal,cor,F,idioma,transform_ord)} else
-              if(res$testes$sup==F | tipox=="ordinal") {res=kruskall(unlist(x),y,nomex,nomey,niveisy,dig,F,excluirtotal,cor,F,idioma,transform_ord)}
+          {res = try(anovac(unlist(x),y,nomex,nomey,niveisy,dig,F,excluirtotal,cor,idioma,virgula=virgula))
+            if(class(res)=="try-error") {res=kruskall(unlist(x),y,nomex,nomey,niveisy,dig,F,excluirtotal,cor,F,idioma,transform_ord,virgula=virgula)} else
+              if(res$testes$sup==F | tipox=="ordinal") {res=kruskall(unlist(x),y,nomex,nomey,niveisy,dig,F,excluirtotal,cor,F,idioma,transform_ord,virgula=virgula)}
           }
        } else
     if(tipoy=="numeric" | tipoy=="ordinal")
       {ordinaly=ifelse(tipoy=="numeric",F,T) ; ordinalx=ifelse(tipox=="numeric",F,T)
-       res=contcont(y,x,nomey,nomex,dig,cor,ordinalx=ordinalx,ordinaly=ordinaly,respcol=respcol)}
+       res=contcont(y,x,nomey,nomex,dig,cor,ordinalx=ordinalx,ordinaly=ordinaly,respcol=respcol,virgula=virgula)}
     }
   else
   {if(tipox=="factor")
     {if(tipoy=="numeric" | tipoy=="ordinal")
       {transform_ord = ifelse(tipoy=="ordinal",T,F)
       if(length(niveisx)==2)
-        {res = try(testet(as.numeric(unlist(y)),x,nomey,nomex,niveisx,dig,T,excluirtotal,cor))
-          if(class(res)=="try-error") {res=mann(x,y,nomex,nomey,niveisx,dig,T,excluirtotal,cor,idioma,transform_ord,niveis_ord=niveisy)} else
-            if(res$testes$sup==F | tipoy=="ordinal") {res=mann(x,y,nomex,nomey,niveisx,dig,T,excluirtotal,cor,idioma,transform_ord,niveis_ord=niveisy)}
+        {res = try(testet(as.numeric(unlist(y)),x,nomey,nomex,niveisx,dig,T,excluirtotal,cor,virgula=virgula))
+          if(class(res)=="try-error") {res=mann(x,y,nomex,nomey,niveisx,dig,T,excluirtotal,cor,idioma,transform_ord,niveis_ord=niveisy,virgula=virgula)} else
+            if(res$testes$sup==F | tipoy=="ordinal") {res=mann(x,y,nomex,nomey,niveisx,dig,T,excluirtotal,cor,idioma,transform_ord,niveis_ord=niveisy,virgula=virgula)}
         } else
-        {res = try(anovac(unlist(y),x,nomey,nomex,niveisx,dig,T,excluirtotal,cor,idioma))
-          if(class(res)=="try-error") {res=kruskall(y,x,nomey,nomex,niveisx,dig,T,excluirtotal,cor,T,idioma,transform_ord)} else
-            if(res$testes$sup==F | tipox=="ordinal") {res=kruskall(y,x,nomey,nomex,niveisx,dig,T,excluirtotal,cor,T,idioma,transform_ord)}
+        {res = try(anovac(unlist(y),x,nomey,nomex,niveisx,dig,T,excluirtotal,cor,idioma,virgula=virgula))
+          if(class(res)=="try-error") {res=kruskall(y,x,nomey,nomex,niveisx,dig,T,excluirtotal,cor,T,idioma,transform_ord,virgula=virgula)} else
+            if(res$testes$sup==F | tipox=="ordinal") {res=kruskall(y,x,nomey,nomex,niveisx,dig,T,excluirtotal,cor,T,idioma,transform_ord,virgula=virgula)}
         }
       } else
-          if(tipoy=="factor") {res=catcat(x,y,nomex,nomey,niveisx,niveisy,dig,respcol,excluirtotal,cor,idioma)}
+          if(tipoy=="factor") {res=catcat(x,y,nomex,nomey,niveisx,niveisy,dig,respcol,excluirtotal,cor,idioma,virgula=virgula)}
     }
   }
   result=res$result
@@ -44,7 +44,7 @@ escolhateste <- function(x,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig=2,exclu
 
 
 
-get_analise_2 <- function(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig=2,excluirtotal=T,respcol=T,cor="cyan4",idioma="PT"){
+get_analise_2 <- function(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig=2,excluirtotal=T,respcol=T,cor="cyan4",idioma="PT",virgula=F){
   xdim = ifelse(is.null(dim(xmat)),1,dim(xmat)[2])
   texto=c() ; textocont=c()
   testes=c()
@@ -54,12 +54,12 @@ get_analise_2 <- function(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig=2,e
     {if(is.null(dim(xmat))==T) xmat_c = data.frame(xmat) else xmat_c = xmat[,i]
     if((tipox[i]=="numeric" | tipox[i]=="ordinal") & (tipoy=="numeric"|tipoy=="ordinal")) {
         temcont=T
-        res=escolhateste(xmat_c,y,tipox[i],tipoy,nomex[i],nomey,eval(parse(text=niveisx[i])),eval(parse(text=niveisy)),dig,excluirtotal,respcol,cor,idioma)
+        res=escolhateste(xmat_c,y,tipox[i],tipoy,nomex[i],nomey,eval(parse(text=niveisx[i])),eval(parse(text=niveisy)),dig,excluirtotal,respcol,cor,idioma,virgula)
         resultcont=rbind(resultcont,res$result)
         textocont=list.append(textocont,res$texto,res$tabela)
         resumo=res$resumo
         testes <- rbind(testes,res$testes)} else
-          {res = escolhateste(xmat_c,y,tipox[i],tipoy,nomex[i],nomey,eval(parse(text=niveisx[i])),eval(parse(text=niveisy)),dig,excluirtotal,respcol,cor,idioma)
+          {res = escolhateste(xmat_c,y,tipox[i],tipoy,nomex[i],nomey,eval(parse(text=niveisx[i])),eval(parse(text=niveisy)),dig,excluirtotal,respcol,cor,idioma,virgula)
           result= rbind(result, res$result)
           texto <- list.append(texto,res$texto)
           testes <- rbind(testes,res$testes)}}
@@ -68,7 +68,7 @@ get_analise_2 <- function(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig=2,e
 return(lista)}
 
 
-get_analise <- function(dados,col,auxiliar,y,tipoy="numeric",nomey="",niveisy="",dig=2,excluirtotal=T,respcol=T,cor='cyan4', idioma="PT"){
+get_analise <- function(dados,col,auxiliar,y,tipoy="numeric",nomey="",niveisy="",dig=2,excluirtotal=T,respcol=T,cor='cyan4', idioma="PT",virgula=F){
 
   if(is.numeric(y)==T & length(y)==1) {
     tipoy=auxiliar$tipo[y]
@@ -80,6 +80,6 @@ get_analise <- function(dados,col,auxiliar,y,tipoy="numeric",nomey="",niveisy=""
   tipox=auxiliar$tipo[col]
   nomex=auxiliar$nomes[col]
   niveisx=auxiliar$niveis[col]
-  res = get_analise_2(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,excluirtotal,respcol,cor,idioma)
+  res = get_analise_2(xmat,y,tipox,tipoy,nomex,nomey,niveisx,niveisy,dig,excluirtotal,respcol,cor,idioma,virgula)
   if(length(res)>6) lista=list("testes"=res$testes,"resumo"=res$resumo,"result"=res$result,"texto"=res$texto,"int"=res$int,"resultcont"=res$resultcont,"textocont"=res$textocont) else lista=list("testes"=res$testes,"resumo"=res$resumo,"result"=res$result,"texto"=res$texto)
 return(lista)}
