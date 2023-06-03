29e51a7789a9b31223234c88d3632db81d0d6d12
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2023-06-03 11:41:49 -0300

    Update categorica_graficoeteste.R

--- R/categorica_graficoeteste.R
+++ R/categorica_graficoeteste.R
@@ -82,7 +82,7 @@ df$lesoes = vetor_comsep_c(df$lesoes,30)
 if(ordenar==T) df = df %>% arrange(desc(-Freq)) %>% mutate(lesoes=factor(lesoes,levels=unique(lesoes))) else
   df = df %>% mutate(lesoes=factor(lesoes,levels=niveis))
   plot = df %>% ggplot(aes(y=lesoes,x=Freq_rel)) + geom_bar(stat="identity", fill=cor) +theme_icic("v") +
-  geom_text(aes(label=ponto_para_virgula(paste0(100*round(Freq_rel,2),"%")),virgula),nudge_x=0.01) + scale_x_continuous(labels = scales::percent_format(), 
+  geom_text(aes(label=ponto_para_virgula(paste0(100*round(Freq_rel,2),"%"),virgula)),nudge_x=0.01) + scale_x_continuous(labels = scales::percent_format(), 
         expand = expansion(mult = c(0, 0.07))) + labs(y = NULL, 
         x = "Proporção", title = vetor_comsep_c(paste(nome, 
             " (n=", length(lesoes), ")", sep = ""), 50))
