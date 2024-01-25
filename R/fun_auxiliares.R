920e4d538f91a70ce0a5be52b44df2008ab7fe80
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2024-01-25 16:03:25 -0300

    Update fun_auxiliares.R

--- R/fun_auxiliares.R
+++ R/fun_auxiliares.R
@@ -151,17 +151,21 @@ arruma_tabela_repetidos = function(vec) {
 rodape_tabela = function(nome,testes){
 
 ana = unique(testes$tipo)
-ana = factor(ana,levels=c("numeric","ordinal","factor","catsame","t","mw","aov","kw","cc","correl","t_par","fried","mcnem"))
+ana = factor(ana,levels=c("numeric","ordinal","factor","catsame","t","mw","aov","kw","cc","correl","t_par","fried","mcnem","qcoch"))
 
 indicador = table(ana)>0
 
 cap = c("Q1-Q3 = Primeiro e terceiro quartis; DP=desvio padrão","","",
   "IC95% = Intervalo de 95% de confiança para proporção",
   "p-valores marcados pela letra 'c' indicam realização do Teste-t e tamanho de efeito d de cohen",
-  "p-valores marcados pela letra 'd' indicam realização do Mann-Whitney e tamanho de efeito r",
+  "p-valores marcados pela letra 'd' indicam realização do teste de Mann-Whitney e tamanho de efeito r",
   "p-valores marcados pela letra 'e' indicam realização do teste ANOVA e tamanho de efeito eta",
   "p-valores marcados pela letra 'f' indicam realização do teste Kruskall-Wallis e tamanho de efeito eta",
-  "p-valores marcados pela letra 'b' indicam realização do teste Exato de Fisher; p-valores marcados pela letra 'a' indicam realização do teste Qui-quadrado, seguido do tamanho de efeito V de cramer")
+  "p-valores marcados pela letra 'b' indicam realização do teste Exato de Fisher; p-valores marcados pela letra 'a' indicam realização do teste Qui-quadrado, seguido do tamanho de efeito V de cramer",
+  "p-valores marcados pela letra 'j' indicam realização do teste Wilcoxon",
+  "p-valores marcados pela letra 'i' indicam realização do teste de Friedman",
+  "p-valores marcados pela letra 'k' indicam realização do teste de McNemar",
+  "p-valores marcados pela letra 'l' indicam realização do teste Q de Cochran")
 
   capt = paste0(cap[indicador], collapse="; ")
 
