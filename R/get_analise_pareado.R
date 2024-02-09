6d695ef811e036586210071f3d278924dc0821e1
icicestatistica <82094622+icicestatistica@users.noreply.github.com>
2024-02-09 15:28:50 -0300

    Update get_analise_pareado.R

--- R/get_analise_pareado.R
+++ R/get_analise_pareado.R
@@ -1,14 +1,14 @@
-escolhateste_pareado = function(id,time,x,tipox,nomex,moms,nometime){
+escolhateste_pareado = function(id,time,x,tipox,nomex,moms,nometime,cor){
 
 result<-data.frame() ; tex="" ; grafico=NULL
 
 n=length(moms)
 if(n==2){
-  if(tipox=="numeric" | tipox=="ordinal") {res=wilcox(id,time,unlist(x),nomex,moms,tipox,nometime)}
+  if(tipox=="numeric" | tipox=="ordinal") {res=wilcox(id,time,unlist(x),nomex,moms,tipox,nometime,cor=cor)}
   else{
     if(tipox=="factor" | tipox=="character") {res=mc_icic(id,time,unlist(x),moms,nomex,nometime)
 }}} else {
-      if(tipox=="numeric") {res=friedman_icic(id,time,unlist(x),nomex,moms,nometime)}
+      if(tipox=="numeric") {res=friedman_icic(id,time,unlist(x),nomex,moms,nometime,cor=cor)}
       else{
         if(tipox=="factor" | tipox=="character") {res=coch_icic(id,time,unlist(x),moms,nomex,nometime)}}
     }
@@ -20,12 +20,12 @@ if(n==2){
               "result"=result,
               "texto"=list("grafico"=grafico,"tex"=tex)))}
 
-get_analise_pareado_2 = function(id,time,xmat,tiposx,nomesx,moms,nometime,nometab){
+get_analise_pareado_2 = function(id,time,xmat,tiposx,nomesx,moms,nometime,nometab,cor){
   xdim = ifelse(is.null(dim(xmat)),1,dim(xmat)[2])
   texto=c();  testes=c() ; result=data.frame()
   if(is.null(dim(xmat))==T) xmat_c = data.frame(xmat) else xmat_c = xmat
   for (i in 1:xdim)
-    {res=escolhateste_pareado(id,time,unlist(xmat_c[,i]),tiposx[i],nomesx[i],moms,nometime)
+    {res=escolhateste_pareado(id,time,unlist(xmat_c[,i]),tiposx[i],nomesx[i],moms,nometime,cor)
         result=rbind(result,res$result)
         texto=list.append(texto,res$texto)
         testes <- rbind(testes,res$testes)}
@@ -34,7 +34,7 @@ get_analise_pareado_2 = function(id,time,xmat,tiposx,nomesx,moms,nometime,nometa
   return(list("testes"=testes,"result"=result,"caption"=capt,"texto"=texto))}
 
 
-get_analise_pareado <- function(data,auxiliar,n_id,n_time,cols,moms,nometime="Momento",nometab="Comparação"){
+get_analise_pareado <- function(data,auxiliar,n_id,n_time,cols,moms,nometime="Momento",nometab="Comparação",cor="cyan4"){
 
   id=unlist(data[,n_id])
   time=unlist(data[,n_time])
@@ -42,7 +42,7 @@ get_analise_pareado <- function(data,auxiliar,n_id,n_time,cols,moms,nometime="Mo
   tiposx=auxiliar$tipo[cols]
   nomesx=auxiliar$nomes[cols]
   
-  res = get_analise_pareado_2(id,time,xmat,tiposx,nomesx,moms,nometime,nometab)
+  res = get_analise_pareado_2(id,time,xmat,tiposx,nomesx,moms,nometime,nometab,cor)
   
   lista=list("testes"=res$testes,"result"=res$result,"caption"=res$caption,"texto"=res$texto)
 return(lista)}
