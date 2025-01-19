36258a4ef4b07c8ba83672b767ecaa277f35f643
Isabelle Carnielli <icicestatistica@gmail.com>
2025-01-18 23:40:50 -0300

    Remover funcoes auxiliares presentes em magicR

--- R/fun_auxiliares.R
+++ R/fun_auxiliares.R
@@ -1,10 +1,4 @@
-pvalor <- function(p){
-  if(is.numeric(p)==F) res="" else
-    if(is.na(p)==T) res="" else
-      if(p>0.05) res = p=formatC(round(p,3), format='f', digits=3) else
-        if(p>0.01) res=paste0(formatC(round(p,3), format='f', digits=3),"\\*") else
-          if(p>=0.001) res=paste0(formatC(round(p,3), format='f', digits=3),"\\*\\*") else res = "<0.001\\*\\*\\*"
-      return(res)}
+
 
 pvetor <- function(vetor){
   pvalores <- c()
@@ -12,29 +6,20 @@ pvetor <- function(vetor){
     pvalores <- c(pvalores,pvalor(vetor[i]))
   return(pvalores)}
 
-printvetor <- function(vetor, idioma="PT",aspas=T){
-size=length(vetor)
-if(size==0) print="" else {
-if(aspas==T) {
-  conec = ifelse(idioma=="PT","' e '","' and '")
-  if(size==1) print=paste0("'",vetor[1],"'") else print=paste(paste("'",vetor[1:(size-1)],collapse="', ",sep=""),conec,vetor[size],"'",collapse=NULL,sep="")} else {
-    conec = ifelse(idioma=="PT"," e "," and ")
-    if(size==1) print=paste0(vetor[1]) else print=paste(paste(vetor[1:(size-1)],collapse=", ",sep=""),conec,vetor[size],collapse=NULL,sep="")}}
-return(print)}
 
 format_real <- function(valor) {
    res=format(round(valor,2),nsmall = 2, decimal.mark = ",", big.mark = ".")
 return(res)}
 
-caixadeselecao = function (vetor, sep = ",", opcoes = "auto") 
+caixadeselecao = function (vetor, sep = ",", opcoes = "auto")
 {
     vetor = unlist(vetor)
-    if (opcoes[1] == "auto") 
+    if (opcoes[1] == "auto")
         opcoes = unique(gdata::trim(unlist(stringr::str_split(vetor, sep))))
     opcoes = opcoes[is.na(opcoes) == F]
-    matrix <- matrix(c(rep("", length(opcoes) * length(vetor))), 
+    matrix <- matrix(c(rep("", length(opcoes) * length(vetor))),
         ncol = length(opcoes))
-    for (i in 1:length(opcoes)) 
+    for (i in 1:length(opcoes))
       for (j in 1:length(vetor))
         if (is.na(vetor[j]) == T) matrix[j, i] = NA else {
             if (opcoes[i] %in% gdata::trim(stringr::str_split(vetor[j],sep)[[1]]))  matrix[j, i] = "Sim"  else matrix[j, i] = "Não"}
@@ -43,7 +28,7 @@ caixadeselecao = function (vetor, sep = ",", opcoes = "auto")
     m <- m[, names(m) != "NA"]
     return(m)
 }
-   
+
 nivcatsame = function(niveisoriginal){
    possi = unique(unlist(str_split(paste0(eval(parse(text=niveisoriginal)),sep=", ",collapse=""),", ")))
    possi = possi[which(possi!="")]
@@ -70,70 +55,13 @@ vec_novo = apply(cx_novo,1,function(x) paste0(novascats[x=="Sim"],collapse=","))
 vec_novo[vec_novo==""]=NA
 return(list("cx_novo"=cx_novo,"vec_novo"=vec_novo))}
 
-                 
 
-vetor_comsep <- function(vec,corte){
-mat = stringr::str_split(vec," ")
-res=c()
-for (i in 1:length(vec)){
-  if (length(mat[[i]]) <= corte) res <- c(res, paste(mat[[i]],collapse=" ",sep="")) else {
-    linhas=ceiling(length(mat[[i]])/corte)
-    if(length(mat[[i]])%%corte==0) ultima = corte else ultima=length(mat[[i]])%%corte
-    aux=c()
-    if (linhas==2) {aux <- c(aux,paste(mat[[i]][1:corte],collapse=" ",sep=""))}
-    if (linhas>2){
-      for (j in 0:(linhas-2)) aux <- c(aux,paste(mat[[i]][j*corte+(1:corte)],collapse=" ",sep=""))}
-    aux <- c(aux, paste(mat[[i]][((linhas-1)*corte)+(1:ultima)],collapse=" ",sep=""))
-  res=c(res, paste(aux, collapse=" \n "))}}
-return(res)}
-   
-vetor_comsep_c <- function(vec,corteinicial){
-res = c()
-    for (vetor in vec) {
-        a = T
-        espacos = c(stringr::str_locate_all(vetor, " ")[[1]][, 1], 
-            nchar(vetor) + 1)
-        inicio = 1
-        corte = corteinicial
-        fim = ifelse(espacos[1] > corte, espacos[1] - 1, espacos[max(which(espacos - 
-            1 <= corte))] - 1)
-        newvec <- NULL
-        ena=FALSE
-        while (a) {
-            if(is.na(vetor)==T) {ena = T; a=F} else {
-            newvec = c(newvec, stringr::str_sub(vetor, inicio, fim))
-            inicio = fim + 2
-            if (inicio > nchar(vetor)) 
-                a = F
-            corte = inicio + corteinicial
-            fim = espacos[max(which(espacos - 1 <= corte))] - 
-                1
-            if (fim < inicio) {
-                fim = espacos[max(which(espacos - 1 <= corte)) + 
-                  1] - 1
-            }
-        }}
-        if(ena) newvec=NA else newvec = paste0(newvec, collapse = "\n")
-        res = c(res, newvec)
-    }
-return(res)}
 
-#vetor_comsep_c(names(dados),50)
 
-relatorio <- function(a,pularprimeiro=T,dig=2){
-  require(knitr)
-if (pularprimeiro==T) comeco=2 else comeco=1
-for (i in comeco:length(a)){
-   if(is.null(a[[i]])==T) tantofaz=0 else {
-if(class(a[[i]])[1]=="data.frame") print(kable(a[[i]], row.names=F,digits=dig)) else
-  if(class(a[[i]])[1]=="list") relatorio(a[[i]],F) else
-      if(class(a[[i]])[1]=="character") cat(a[[i]], sep="\n") else
-        print(a[[i]])}}}
-   
 cont_analises <- function(nome,a){
 df = data.frame("Sessão"=rep(nome,dim(a$testes)[1]),a$testes)
 return(df)}
-   
+
 transpordf <- function(tab){
 nova=data.frame(t(tab))
 nova <- cbind(names(tab),nova)
@@ -145,7 +73,7 @@ return(nova)}
 vec_to_string <- function(vec){
    str = paste0("c(",paste(paste("'",vec,"'",sep=""),collapse=","),")",collapse="")
    return(str)}
-      
+
 turn_into_factor=function(vec,niveis_str){
   return(unname(factor(unlist(vec),levels=eval(parse(text=niveis_str)))))}
 
@@ -153,16 +81,11 @@ Transformar = function(vec,original,novo){
 a=factor(vec,levels=original)
 levels(a)=novo
 return(a)}
-     
-quiet <- function(x) { 
-  sink(tempfile()) 
-  on.exit(sink()) 
+
+quiet <- function(x) {
+  sink(tempfile())
+  on.exit(sink())
   invisible(force(x))}
-     
-ponto_para_virgula = function (vetor, virgula = F) {
-    if(virgula==T) res = str_replace_all(vetor, "\\.", 
-        "\\,") else res=vetor
-    return(res)}
 
 arruma_tabela_repetidos = function(vec) {
   fica = unlist(lapply(1:length(unique(vec)),function(x) min(which(str_detect(vec, fixed(unique(vec)[x]))==T))))
@@ -186,7 +109,7 @@ cap = c("Q1-Q3 = Primeiro e terceiro quartis; DP=desvio padrão","","",
   "p-valores marcados pela letra 'b' indicam realização do teste Exato de Fisher",
   "p-valores marcados pela letra 'a' indicam realização do teste Qui-quadrado, seguido do tamanho de efeito V de cramer",
   "",
-  "",      
+  "",
   "p-valores marcados pela letra 'j' indicam realização do teste Wilcoxon",
   "p-valores marcados pela letra 'i' indicam realização do teste de Friedman",
   "p-valores marcados pela letra 'k' indicam realização do teste de McNemar",
@@ -204,9 +127,9 @@ return(capt)}
 get_niveis = function(vec) {return(vec_to_string(names(table(vec))))}
 
 ###################
-                       
+
 preencher = function(vec,vazios=""){
-  
+
 if(is.na(vazios)) {vec[is.na(vec)]=""; vazios=""}
 
 id_vaz = which(vec==vazios)
@@ -217,4 +140,4 @@ mudar = sapply(id_vaz, function(x) max(id_nvaz[id_nvaz<x]))
 vec_novo = vec
 vec_novo[id_vaz] = vec[mudar]
 
-return(vec_novo)}                       
+return(vec_novo)}
